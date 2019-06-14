695d9671dc5d8f2fd8f68c8a67c4bdc157f079b6
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-06-14 14:35:31 +0200

    Reworked container-styling

---
+++ src/app/authorize/net-worth/components/net-worth-bar/net-worth-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-net-worth-bar',
+  templateUrl: './net-worth-bar.component.html',
+  styleUrls: ['./net-worth-bar.component.scss']
+})
+export class NetWorthBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/authorize/net-worth/net-worth.module.ts
+++ src/app/authorize/net-worth/net-worth.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { NetWorthComponent } from './net-worth.component';
 import { MatTabsModule, MatIconModule } from '@angular/material';
+import { NetWorthBarComponent } from './components/net-worth-bar/net-worth-bar.component';
 
 @NgModule({
-  declarations: [NetWorthComponent],
+  declarations: [NetWorthComponent, NetWorthBarComponent],
   imports: [
     SharedModule,
     MatTabsModule,
