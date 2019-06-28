ddb74c6c1148baa2c4f58a2c5f25856a950a9b96
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-06-28 18:50:58 +0200

    Reworks to chart

--- ExilenceNextClient/src/app/auth/net-worth/components/net-worth-graph/net-worth-graph.component.ts
+++ ExilenceNextClient/src/app/auth/net-worth/components/net-worth-graph/net-worth-graph.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as moment from 'moment';
-import { ChartSeries, ChartSeriesEntry } from '../../../../shared/interfaces/chart.interface';
 
 @Component({
   selector: 'app-net-worth-graph',
@@ -8,18 +7,7 @@ import { ChartSeries, ChartSeriesEntry } from '../../../../shared/interfaces/cha
   styleUrls: ['./net-worth-graph.component.scss']
 })
 export class NetWorthGraphComponent implements OnInit {
-  // todo: remove mock data
-
-  public data = [
-    [1, 100, 90, 110, 85, 96, 104, 120],
-    [2, 120, 95, 130, 90, 113, 124, 140],
-    [3, 130, 105, 140, 100, 117, 133, 139],
-    [4, 90, 85, 95, 85, 88, 92, 95],
-    [5, 70, 74, 63, 67, 69, 70, 72],
-    [6, 30, 39, 22, 21, 28, 34, 40],
-    [7, 80, 77, 83, 70, 77, 85, 90],
-    [8, 100, 90, 110, 85, 95, 102, 110]
-  ];
+  @Input() data: Array<any[]>;
 
   options = {
     colors: ['#e91e63', '#fff'],
@@ -40,9 +28,8 @@ export class NetWorthGraphComponent implements OnInit {
     },
     legend: 'none',
     chartArea: { width: '90%', height: '85%' },
-    curveType: 'function',
-    series: [{ 'color': '#e91e63' }],
-    intervals: { style: 'bars', color: '#fff' },
+    curveType: 'function'
+    // series: [{ 'color': '#e91e63' }],
   };
   // moment(new Date()).add(1, 'days').toDate()   #e91e63
 
--- ExilenceNextClient/src/app/auth/net-worth/containers/net-worth-page/net-worth-page.component.ts
+++ ExilenceNextClient/src/app/auth/net-worth/containers/net-worth-page/net-worth-page.component.ts
@@ -7,6 +7,7 @@ import { Store } from '@ngrx/store';
 import { NetWorthStatus } from '../../../../shared/interfaces/net-worth-status.interface';
 import { Observable } from 'rxjs';
 import { NetWorthState } from '../../../../app.states';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-net-worth-page',
@@ -15,6 +16,19 @@ import { NetWorthState } from '../../../../app.states';
 })
 export class NetWorthPageComponent implements OnInit {
   public selectedIndex = 0;
+  
+  // todo: remove mock data
+  public data = [
+    [moment(new Date()).add(1, 'hours').toDate(), 1],
+    [moment(new Date()).add(2, 'hours').toDate(), 2],
+    [moment(new Date()).add(3, 'hours').toDate(), 2.5],
+    [moment(new Date()).add(4, 'hours').toDate(), 2.7],
+    [moment(new Date()).add(5, 'hours').toDate(), 3],
+    [moment(new Date()).add(6, 'hours').toDate(), 7],
+    [moment(new Date()).add(7, 'hours').toDate(), 10],
+    [moment(new Date()).add(8, 'hours').toDate(), 2],
+    [moment(new Date()).add(9, 'hours').toDate(), 3]
+  ];
 
   @ViewChild('tabGroup', undefined) tabGroup: MatTabGroup;
 
