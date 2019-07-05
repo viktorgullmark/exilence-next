2b0b96bf7590382b461c3680f67a760bc3696c24
Viktor Gullmark <viktor.gullmark@hotmail.com>
2019-07-05 09:44:25 +0200

    Formatting

--- ExilenceNextClient/src/app/shared/helpers/snapshot.helper.ts
+++ ExilenceNextClient/src/app/shared/helpers/snapshot.helper.ts
@@ -5,17 +5,17 @@ import { CompactTab } from '../interfaces/stash.interface';
 export class SnapshotHelper {
     public static formatSnapshotsForChart(compactTabs: CompactTab[], snapshots: Snapshot[]): Array<ChartSeries> {
         const chartSeries: ChartSeries[] = compactTabs.map(tab => {
-            return { name: tab.i + ' - ' + tab.n, series: [], id: tab.id } as ChartSeries
+            return { name: tab.i + ' - ' + tab.n, series: [], id: tab.id } as ChartSeries;
         });
 
         for (let i = 0; i < snapshots.length; i++) {
             for (let j = 0; j < snapshots[i].tabSnapshots.length; j++) {
-                const tabSeries = chartSeries.find(cs => cs.id === snapshots[i].tabSnapshots[j].tabId);     
+                const tabSeries = chartSeries.find(cs => cs.id === snapshots[i].tabSnapshots[j].tabId);
                 if (tabSeries !== undefined) {
                     tabSeries.series.push({ name: snapshots[i].timestamp, value: snapshots[i].tabSnapshots[j].value } as ChartSeriesEntry);
                     chartSeries[chartSeries.indexOf(tabSeries)] = tabSeries;
                 }
-            }         
+            }
         }
 
         return chartSeries;
