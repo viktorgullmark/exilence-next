9e38c7a71d93bb3dba461b0e3f76111e9a22c02b
Petter Annerwall <petterannerwall@gmail.it>
2019-07-09 11:45:10 +0200

    Improved error handling

--- ExilenceNextClient/src/app/core/error-handling/error-handler.ts
+++ ExilenceNextClient/src/app/core/error-handling/error-handler.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpError } from '../../shared/interfaces/http-error.interface';
+import { ParsedUrl } from '../../shared/interfaces/parsed-url.interface';
 
 @Injectable()
 export class ErrorHandler {
@@ -9,12 +11,18 @@ export class ErrorHandler {
         private translateService: TranslateService
     ) { }
 
-    public handleError(err: any) {
-        switch (err.status) {
-            case 403: {
-                err.message = this.translateService.instant('ERROR.UNAUTHORIZED');
-                break;
-            }
+  public handleError(err: HttpError) {
+    switch (err.status) {
+
+      case 0: {
+        err.message = this.translateService.instant('ERROR.UNREACHABLE') + this.parseUrl(err.url).origin;
+        break;
+      }
+
+      case 403: {
+        err.message = this.translateService.instant('ERROR.UNAUTHORIZED');
+        break;
+      }
 
             case 404: {
                 err.message = this.translateService.instant('ERROR.NOT_FOUND');
@@ -26,6 +34,15 @@ export class ErrorHandler {
             }
         }
 
-        this.snackbar.open(err.message, 'close');
-    }
+    this.snackbar.open(err.message, 'close');
+  }
+
+
+  private parseUrl(url: string): ParsedUrl {
+    const a = document.createElement('a');
+    a.setAttribute('href', url);
+    const { host, hostname, pathname, port, protocol, search, hash } = a;
+    const origin = `${protocol}//${hostname}${port.length ? `:${port}` : ''}`;
+    return { origin, host, hostname, pathname, port, protocol, search, hash };
+  }
 }
---
+++ ExilenceNextClient/src/app/shared/interfaces/parsed-url.interface.ts
@@ -0,0 +1,10 @@
+export interface ParsedUrl {
+  origin: string;
+  host: string;
+  hostname: string;
+  pathname: string;
+  port: string;
+  protocol: string;
+  search: string;
+  hash: string;
+}
