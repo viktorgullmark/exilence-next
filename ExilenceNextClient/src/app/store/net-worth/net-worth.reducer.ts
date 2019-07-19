d7524a5fdee33dad87cbf6ea83666b26bb2e5598
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-07-19 16:20:47 +0200

    Fixes to disabled attr

--- ExilenceNextClient/src/app/store/net-worth/net-worth.reducer.ts
+++ ExilenceNextClient/src/app/store/net-worth/net-worth.reducer.ts
@@ -15,10 +15,12 @@ export function reducer(
         ...state,
         ...action.payload.state,
         status: {
+          ...state.status,
           ...action.payload.state.status,
           snapshotting: false
         },
         stash: {
+          ...state.stash,
           ...action.payload.state.stash,
           tabCount: 0,
           tabCountFetched: 0
