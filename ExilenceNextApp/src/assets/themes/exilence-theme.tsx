0848a4c5f631d94673b598569e28348a320992f2
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-10-03 09:33:49 +0200

    Adjustments to header styles

--- ExilenceNextApp/src/assets/themes/exilence-theme.tsx
+++ ExilenceNextApp/src/assets/themes/exilence-theme.tsx
@@ -1,8 +1,20 @@
 import createMuiTheme, { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 import { Palette } from '@material-ui/core/styles/createPalette';
 
+const defaultTheme = createMuiTheme();
+
 export default function exilenceTheme() {
     return createMuiTheme({
+        overrides: {
+            MuiToolbar: {
+              gutters: {
+                [defaultTheme.breakpoints.up('sm')]: {
+                  paddingLeft: '8px',
+                  paddingRight: '8px',
+                },
+              },
+            },
+          },
         palette: {
             primary: {
                 light: '#fff',
--- ExilenceNextApp/src/components/header/Header.tsx
+++ ExilenceNextApp/src/components/header/Header.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import classes from '*.module.css';
+import { AppBar, Button, makeStyles, Toolbar } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import React from 'react';
+
+const useStyles = makeStyles({
+  toolbar: {
+    minHeight: 40
+  },
+});
 
 const Header: React.FC = () => {
 
+  const classes = useStyles();
+
   return (
     <AppBar position="fixed">
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <Grid
           container
           direction="row"
