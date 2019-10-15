2216ea21ad0f93e0d564f171146daea23b3dade8
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-10-15 17:20:20 +0200

    Implemented basic auth service

--- ExilenceNextApp/src/index.tsx
+++ ExilenceNextApp/src/index.tsx
@@ -4,44 +4,21 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import exilenceTheme from './assets/themes/exilence-theme';
 import App from './core/app/App';
-import Admin from './routes/admin/Admin';
+import configureI18n from './i18n';
+import NetWorth from './routes/net-worth/NetWorth';
 import Login from './routes/login/Login';
 import configureStore from './store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { initReactI18next } from "react-i18next";
-import Backend from 'i18next-xhr-backend';
-import i18n from 'i18next'
-import AppConfig from './config/app.config';
+import { authService } from './services/auth.service';
+
+configureI18n();
 
 const theme = responsiveFontSizes(exilenceTheme());
 
 const { store, persistor } = configureStore();
-
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: 'en',
-    backend: {
-      /* translation file path */
-      loadPath: AppConfig.i18nUrl,
-      crossDomain: true
-    },
-    fallbackLng: 'en',
-    debug: !AppConfig.production,
-    ns: ['general'],
-    defaultNS: 'general',
-    keySeparator: false,
-    interpolation: {
-      escapeValue: false,
-      formatSeparator: ','
-    },
-    react: {
-      wait: true
-    }
-  });
 
 const routing = (
   <Suspense fallback={null}>
@@ -50,14 +27,13 @@ const routing = (
         <ThemeProvider theme={theme}>
           <Router>
             <Route path="/" component={App} />
-            <Redirect from="/" to="/admin" />
+            <Redirect from="/" to="/net-worth" />
             <Route path="/login" component={Login} />
-            <Route path="/admin" render={() => (
-              // todo: implement proper auth check
-              true ? (
+            <Route path="/net-worth" render={() => (
+              !authService.isLoggedIn() ? (
                 <Redirect to="/login" />
               ) : (
-                  <Admin />
+                  <NetWorth />
                 )
             )} />
           </Router>
