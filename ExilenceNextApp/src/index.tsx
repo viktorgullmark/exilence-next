44269494d1ac9c7a6701e8e3a59d36330bb085b5
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-10-16 13:17:50 +0200

    Added localforage config

--- ExilenceNextApp/src/index.tsx
+++ ExilenceNextApp/src/index.tsx
@@ -1,25 +1,33 @@
+import { CssBaseline } from '@material-ui/core';
 import { responsiveFontSizes } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import localForage from 'localforage';
+import { enableLogging } from 'mobx-logger';
+import { Provider } from 'mobx-react';
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 
 import exilenceTheme from './assets/themes/exilence-theme';
-import configureI18n from './i18n';
-import NetWorth from './routes/net-worth/NetWorth';
-import Login from './routes/login/Login';
-import { authService } from './services/auth.service';
-import { Provider } from 'mobx-react'
-import { SessionStore } from './store/session/store';
-import { CssBaseline } from '@material-ui/core';
-import GlobalStyles from './core/global-styles/GlobalStyles';
 import HeaderContainer from './components/header/HeaderContainer';
-import { enableLogging } from 'mobx-logger';
+import GlobalStyles from './core/global-styles/GlobalStyles';
+import configureI18n from './i18n';
+import Login from './routes/login/Login';
+import NetWorth from './routes/net-worth/NetWorth';
+import { authService } from './services/auth.service';
+import { SessionStore } from './store/session/store';
 
 enableLogging();
 configureI18n();
 
 const theme = responsiveFontSizes(exilenceTheme());
+
+localForage.config({
+  name: 'exilence-next-db',
+  driver: localForage.INDEXEDDB,
+});
+
+// todo: implement middleware to persist store with localforage
 
 const sessionStore = new SessionStore();
 
@@ -48,4 +56,8 @@ const app = (
     </ThemeProvider>
   </React.Fragment>
 )
+
 ReactDOM.render(app, document.getElementById('root'))
+
+
+
