cbcf40deb9ed3f7ac98a7fd5e5a5684d5303537f
Petter Annerwall <petterannerwall@gmail.it>
2019-10-17 16:28:28 +0200

    Updates

--- ExilenceNextBackend/src/api.effects.ts
+++ ExilenceNextBackend/src/api.effects.ts
@@ -1,7 +1,7 @@
-import { r } from '@marblejs/core';
+import { HttpRequest, r, RouteEffect } from '@marblejs/core';
 import { mapTo } from 'rxjs/operators';
 
-export const api$ = r.pipe(
+export const api$: RouteEffect<HttpRequest> = r.pipe(
   r.matchPath('/'),
   r.matchType('GET'),
   r.useEffect(req$ => req$.pipe(
