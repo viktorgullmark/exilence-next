3db13861e1e170d1d11a28ba77f734c0d59bd4db
Petter Annerwall <petterannerwall@gmail.it>
2019-10-17 17:25:57 +0200

    Added initial commit of typeorm

---
+++ ExilenceNextBackend/src/orm-index.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
+
+createConnection().then(async connection => {
+
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
