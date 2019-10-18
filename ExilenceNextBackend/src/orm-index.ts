e3e9c979145aac32c38a67955acb360821201208
Petter Annerwall <petterannerwall@gmail.it>
2019-10-18 14:47:22 +0200

    Tweaked to working typeorm

--- ExilenceNextBackend/src/orm-index.ts
+++ ExilenceNextBackend/src/orm-index.ts
@@ -1,19 +1,18 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
+import { Account } from "./entity/Account";
+
 
 createConnection().then(async connection => {
 
     console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+    const account = new Account();
+    account.name = "Timber";
+    await connection.manager.save(account);
+    console.log("Saved a new user with id: " + account.id);
 
     console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
+    const users = await connection.manager.find(Account);
     console.log("Loaded users: ", users);
 
     console.log("Here you can setup and run express/koa/any other framework.");
