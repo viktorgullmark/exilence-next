071df91e9c0e05787032503fa6acefd7a0117f52
Petter Annerwall <petterannerwall@gmail.it>
2019-11-23 16:10:27 +0100

    Fix

--- ExilenceNextBackend/API/Hubs/GroupHub.cs
+++ ExilenceNextBackend/API/Hubs/GroupHub.cs
@@ -25,20 +25,20 @@ namespace API.Hubs
                     group = new Group(name, new List<Connection>() { connection });
                     group = await _groupRepository.AddGroup(group);
                 }
-                else if(!group.Connections.Any(c => c.ConnectionId == connection.ConnectionId))
+                else if (!group.Connections.Any(c => c.ConnectionId == connection.ConnectionId))
                 {
                     group.Connections.Add(connection);
                 }
                 await _groupRepository.SaveChangesAsync();
                 await Groups.AddToGroupAsync(Context.ConnectionId, name);
                 await Log($"Added connectionId: {connection.ConnectionId} to group: {group.Name}");
+                await Clients.Caller.SendAsync("JoinGroup", group);
             }
             catch (Exception e)
             {
                 await Log(e.Message);
-                await Clients.Caller.SendAsync("JoinGroup", false);
+                await Clients.Caller.SendAsync("JoinGroup", e);
             }
-            await Clients.Caller.SendAsync("JoinGroup", true);
         }
 
         public async Task LeaveGroup(string name)
