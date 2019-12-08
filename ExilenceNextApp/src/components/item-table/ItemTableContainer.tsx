b5e9fab0c067796e927409737566279d10f96986
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-12-08 15:23:45 +0100

    Added item filter to ItemTable

--- ExilenceNextApp/src/components/item-table/ItemTableContainer.tsx
+++ ExilenceNextApp/src/components/item-table/ItemTableContainer.tsx
@@ -1,22 +1,72 @@
 import { inject, observer } from 'mobx-react';
-import React, { useState } from 'react';
+import React, { useState, FormEvent, ChangeEvent } from 'react';
 import { UiStateStore } from '../../store/uiStateStore';
 import ItemTable from './ItemTable';
 import { AccountStore } from '../../store/accountStore';
+import ItemTableFilter from './item-table-filter/ItemTableFilter';
+import { IPricedItem } from '../../interfaces/priced-item.interface';
+import { Grid, Box, makeStyles, Theme } from '@material-ui/core';
 
 interface ItemTableContainerProps {
   uiStateStore?: UiStateStore;
   accountStore?: AccountStore;
 }
 
+export const itemTableFilterHeight = 48;
+export const itemTableFilterSpacing = 2;
+
+const useStyles = makeStyles((theme: Theme) => ({
+  itemTableFilter: {
+    height: itemTableFilterHeight
+  }
+}));
+
 const ItemTableContainer: React.FC<ItemTableContainerProps> = ({
   accountStore,
   uiStateStore
 }: ItemTableContainerProps) => {
+  const { tableItems } = accountStore!.getSelectedAccount.activeProfile;
+  const classes = useStyles();
+  const [filteredItems, setFilteredItems] = useState<IPricedItem[]>(tableItems);
 
-  const tableItems = accountStore!.getSelectedAccount.activeProfile.tableItems;
-  
-  return <ItemTable items={tableItems} pageIndex={uiStateStore!.itemTablePageIndex} changePage={(i: number) => uiStateStore!.changeItemTablePage(i)}/>;
+  let timer: NodeJS.Timeout | undefined = undefined;
+
+  const handleFilter = (
+    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      const filteredItems = tableItems.filter(ti =>
+        ti.name.toLowerCase().includes(event.target.value.toLowerCase())
+      );
+      setFilteredItems(filteredItems);
+    }, 500);
+  };
+
+  return (
+    <>
+      <Box mb={itemTableFilterSpacing} className={classes.itemTableFilter}>
+        <Grid container>
+          <Grid item xs={3}>
+            <ItemTableFilter
+              array={filteredItems}
+              handleFilter={handleFilter}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+      <ItemTable
+        items={filteredItems}
+        pageIndex={uiStateStore!.itemTablePageIndex}
+        changePage={(i: number) => uiStateStore!.changeItemTablePage(i)}
+      />
+    </>
+  );
 };
 
-export default inject('uiStateStore', 'accountStore')(observer(ItemTableContainer));
+export default inject(
+  'uiStateStore',
+  'accountStore'
+)(observer(ItemTableContainer));
---
+++ ExilenceNextApp/src/components/item-table/item-table-filter/ItemTableFilter.tsx
@@ -0,0 +1,60 @@
+import { TextField, makeStyles, Theme } from '@material-ui/core';
+import { Formik } from 'formik';
+import React, { ChangeEvent } from 'react';
+import { useTranslation } from 'react-i18next';
+import * as Yup from 'yup';
+import { IPricedItem } from '../../../interfaces/priced-item.interface';
+
+export interface TableFilterProps<T> {
+  array: T[];
+  handleFilter: (
+    event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => void;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  searchField: {
+    width: '100%'
+  }
+}));
+
+const ItemTableFilter: React.FC<TableFilterProps<IPricedItem>> = (
+  props: TableFilterProps<IPricedItem>
+) => {
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  return (
+    <Formik
+      initialValues={{
+        searchText: ''
+      }}
+      onSubmit={(values: { searchText: string }) => {
+        console.log(values);
+      }}
+      validationSchema={Yup.object().shape({
+        searchText: Yup.string().max(20)
+      })}
+    >
+      {formProps => {
+        const { values, handleChange } = formProps;
+
+        return (
+          <form>
+            <TextField
+              onChange={e => {
+                handleChange(e);
+                props.handleFilter(e);
+              }}
+              id="search-text"
+              label={t('tables:label.search_text')}
+              className={classes.searchField}
+            />
+          </form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default ItemTableFilter;
