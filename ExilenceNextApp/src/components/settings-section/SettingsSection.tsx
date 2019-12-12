4f0cdacb3a34544b77477c18af3a08998e87e365
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-12-12 17:00:05 +0100

    Started working on base for settings-page

---
+++ ExilenceNextApp/src/components/settings-section/SettingsSection.tsx
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { observer } from 'mobx-react';
+import React from 'react';
+import { Typography, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary
+  },
+  title: {}
+}));
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  title: string;
+}
+
+const SettingsSection: React.FC<Props> = ({ children, title }: Props) => {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h6" className={classes.title}>
+        {title}
+      </Typography>
+      {children}
+    </Paper>
+  );
+};
+
+export default observer(SettingsSection);
--- ExilenceNextApp/src/routes/settings/Settings.tsx
+++ ExilenceNextApp/src/routes/settings/Settings.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import { Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import SettingsSection from '../../components/settings-section/SettingsSection';
+import FeatureWrapper from '../../components/feature-wrapper/FeatureWrapper';
 
 const Settings: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <Typography variant="h1">{t('title.settings')}</Typography>
-    </>
+    <FeatureWrapper>
+      <SettingsSection title={t('title.net_worth_settings')}></SettingsSection>
+    </FeatureWrapper>
   );
 };
 
