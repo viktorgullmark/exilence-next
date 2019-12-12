cadd574b70f577b67e18d58f714e35acfacc7deb
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-12-12 17:34:02 +0100

    Basic grid structure

--- ExilenceNextApp/src/routes/settings/Settings.tsx
+++ ExilenceNextApp/src/routes/settings/Settings.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import SettingsSection from '../../components/settings-section/SettingsSection';
 import FeatureWrapper from '../../components/feature-wrapper/FeatureWrapper';
@@ -10,7 +10,16 @@ const Settings: React.FC = () => {
 
   return (
     <FeatureWrapper>
-      <SettingsSection title={t('title.net_worth_settings')}></SettingsSection>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <SettingsSection
+            title={t('title.net_worth_settings')}
+          ></SettingsSection>
+        </Grid>
+        <Grid item xs={12}>
+          <SettingsSection title={t('title.window_settings')}></SettingsSection>
+        </Grid>
+      </Grid>
     </FeatureWrapper>
   );
 };
