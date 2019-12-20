6c294ba4a3939707f7149bc907a032e86f91f20d
Viktor Gullmark <viktor.gullmark@gmail.com>
2019-12-20 10:33:25 +0100

    Configured sentry to ignore common network errors

--- ExilenceNextApp/src/config/sentry.ts
+++ ExilenceNextApp/src/config/sentry.ts
@@ -4,7 +4,19 @@ import AppConfig from './app.config';
 function initSentry() {
   if (AppConfig.production) {
     Sentry.init({
-      dsn: AppConfig.sentryBrowserDsn
+      dsn: AppConfig.sentryBrowserDsn,
+      ignoreErrors: [
+        'https://www.pathofexile.com',
+        'Network Error',
+        'NetworkError',
+        'net::ERR_CONNECTION_TIMED_OUT',
+        'net::ERR_NAME_RESOLUTION_FAILED',
+        'net::ERR_NETWORK_IO_SUSPENDED',
+        'net::ERR_NETWORK_CHANGED',
+        'net::ERR_NAME_NOT_RESOLVED',
+        'net::ERR_CONNECTION_RESET',
+        'net::ERR_TIMED_OUT'
+      ]
     });
   }
 }
