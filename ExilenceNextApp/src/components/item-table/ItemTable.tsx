27a6fde754d3032dea8e04e9da25a0742b0f464d
NAn <Niklas.abrahamsson@exor.se>
2019-12-20 10:01:17 +0100

    Removed misplaced code

--- ExilenceNextApp/src/components/item-table/ItemTable.tsx
+++ ExilenceNextApp/src/components/item-table/ItemTable.tsx
@@ -1,45 +1,45 @@
-import Paper from '@material-ui/core/Paper';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import clsx from 'clsx';
-import { observer } from 'mobx-react';
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import uuid from 'uuid';
-import { IColumn } from '../../interfaces/column.interface';
-import { IPricedItem } from '../../interfaces/priced-item.interface';
-import { netWorthGridSpacing } from '../../routes/net-worth/NetWorth';
-import { resizeHandleContainerHeight, toolbarHeight } from '../header/Header';
-import { netWorthTabGroupHeight } from '../net-worth-tab-group/NetWorthTabGroup';
-import { tabPanelSpacing } from '../tab-panel/TabPanel';
-import { innerToolbarHeight } from '../toolbar/Toolbar';
-import { cardHeight } from '../widget/Widget';
-import ItemTableCell from './item-table-cell/ItemTableCell';
-import ItemTableHeader from './item-table-header/ItemTableHeader';
+import Paper from "@material-ui/core/Paper";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import clsx from "clsx";
+import { observer } from "mobx-react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import uuid from "uuid";
+import { IColumn } from "../../interfaces/column.interface";
+import { IPricedItem } from "../../interfaces/priced-item.interface";
+import { netWorthGridSpacing } from "../../routes/net-worth/NetWorth";
+import { resizeHandleContainerHeight, toolbarHeight } from "../header/Header";
+import { netWorthTabGroupHeight } from "../net-worth-tab-group/NetWorthTabGroup";
+import { tabPanelSpacing } from "../tab-panel/TabPanel";
+import { innerToolbarHeight } from "../toolbar/Toolbar";
+import { cardHeight } from "../widget/Widget";
+import ItemTableCell from "./item-table-cell/ItemTableCell";
+import ItemTableHeader from "./item-table-header/ItemTableHeader";
 import {
   itemTableFilterHeight,
   itemTableFilterSpacing
-} from './ItemTableContainer';
+} from "./ItemTableContainer";
 
 export const tableFooterHeight = 52;
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: '100%',
+      width: "100%",
       height: `calc(100vh - ${toolbarHeight}px - ${resizeHandleContainerHeight}px - ${innerToolbarHeight}px - ${cardHeight}px - ${itemTableFilterHeight}px - ${theme.spacing(
         netWorthGridSpacing * 3 + tabPanelSpacing * 2 + itemTableFilterSpacing
       )}px - ${netWorthTabGroupHeight}px)`
     },
     tableWrapper: {
-      overflow: 'auto',
+      overflow: "auto",
       height: `calc(100% - ${tableFooterHeight}px)`
     },
     noItems: {
-      height: 'auto'
+      height: "auto"
     },
     pagination: {
       height: tableFooterHeight,
@@ -47,19 +47,19 @@ export const useStyles = makeStyles((theme: Theme) =>
     },
     visuallyHidden: {
       border: 0,
-      clip: 'rect(0 0 0 0)',
+      clip: "rect(0 0 0 0)",
       height: 1,
       margin: -1,
-      overflow: 'hidden',
+      overflow: "hidden",
       padding: 0,
-      position: 'absolute',
+      position: "absolute",
       top: 20,
       width: 1
     }
   })
 );
 
-export type Order = 'asc' | 'desc';
+export type Order = "asc" | "desc";
 
 interface ItemTableProps {
   items: IPricedItem[];
@@ -73,58 +73,58 @@ const ItemTable: React.FC<ItemTableProps> = ({
   changePage
 }: ItemTableProps) => {
   const classes = useStyles();
-  const { t } = useTranslation(['tables']);
-  const [orderBy, setOrderBy] = useState<keyof IPricedItem>('name');
-  const [order, setOrder] = useState<Order>('asc');
+  const { t } = useTranslation(["tables"]);
+  const [orderBy, setOrderBy] = useState<keyof IPricedItem>("name");
+  const [order, setOrder] = useState<Order>("asc");
 
   const columns: IColumn[] = [
     {
-      id: 'icon',
-      label: t('tables:header.icon'),
+      id: "icon",
+      label: t("tables:header.icon"),
       minWidth: 100,
       maxWidth: 140
     },
-    { id: 'name', label: t('tables:header.name'), minWidth: 50, maxWidth: 220 },
+    { id: "name", label: t("tables:header.name"), minWidth: 50, maxWidth: 220 },
     {
-      id: 'corrupted',
-      label: t('tables:header.corrupted'),
+      id: "corrupted",
+      label: t("tables:header.corrupted"),
       minWidth: 60,
       maxWidth: 100
     },
     {
-      id: 'links',
-      label: t('tables:header.links'),
+      id: "links",
+      label: t("tables:header.links"),
       minWidth: 60,
       numeric: true
     },
     {
-      id: 'quality',
-      label: t('tables:header.quality'),
+      id: "quality",
+      label: t("tables:header.quality"),
       minWidth: 60,
       numeric: true
     },
     {
-      id: 'level',
-      label: t('tables:header.level'),
+      id: "level",
+      label: t("tables:header.level"),
       minWidth: 60,
       numeric: true
     },
     {
-      id: 'stackSize',
-      label: t('tables:header.stacksize'),
+      id: "stackSize",
+      label: t("tables:header.stacksize"),
       minWidth: 60,
       numeric: true
     },
     {
-      id: 'calculated',
-      label: t('tables:header.calculated'),
+      id: "calculated",
+      label: t("tables:header.calculated"),
       minWidth: 60,
       format: (value: number) => value.toFixed(2),
       numeric: true
     },
     {
-      id: 'total',
-      label: t('tables:header.total'),
+      id: "total",
+      label: t("tables:header.total"),
       minWidth: 60,
       format: (value: number) => value.toFixed(2),
       numeric: true
@@ -142,7 +142,6 @@ const ItemTable: React.FC<ItemTableProps> = ({
 
   const handleChangePage = (event: unknown, newPage: number) => {
     changePage(newPage);
-    pageIndex = 1;
   };
 
   function desc<T>(a: T, b: T, orderBy: keyof T) {
@@ -172,7 +171,7 @@ const ItemTable: React.FC<ItemTableProps> = ({
     a: { [key in K]: number | string | boolean },
     b: { [key in K]: number | string | boolean }
   ) => number {
-    return order === 'desc'
+    return order === "desc"
       ? (a, b) => desc(a, b, orderBy)
       : (a, b) => -desc(a, b, orderBy);
   }
@@ -181,8 +180,8 @@ const ItemTable: React.FC<ItemTableProps> = ({
     event: React.MouseEvent<unknown>,
     property: keyof IPricedItem
   ) => {
-    const isDesc = orderBy === property && order === 'desc';
-    setOrder(isDesc ? 'asc' : 'desc');
+    const isDesc = orderBy === property && order === "desc";
+    setOrder(isDesc ? "asc" : "desc");
     setOrderBy(property);
   };
 
@@ -226,7 +225,7 @@ const ItemTable: React.FC<ItemTableProps> = ({
                             key={uuid.v4()}
                             column={column}
                             value={row[column.id]}
-                            frameType={row['frameType']}
+                            frameType={row["frameType"]}
                           />
                         );
                       })}
@@ -244,10 +243,10 @@ const ItemTable: React.FC<ItemTableProps> = ({
           rowsPerPage={rowsPerPage}
           page={pageIndex}
           backIconButtonProps={{
-            'aria-label': 'previous page'
+            "aria-label": "previous page"
           }}
           nextIconButtonProps={{
-            'aria-label': 'next page'
+            "aria-label": "next page"
           }}
           onChangePage={handleChangePage}
           onChangeRowsPerPage={handleChangeRowsPerPage}
