a6971b3d848983ccf4ac244a5e74b2baa942c9a4
Viktor Gullmark <viktor.gullmark@gmail.com>
2020-01-26 01:28:17 +0100

    Chart improvements

--- ExilenceNextApp/src/components/snapshot-history-chart/SnapshotHistoryChart.tsx
+++ ExilenceNextApp/src/components/snapshot-history-chart/SnapshotHistoryChart.tsx
@@ -8,6 +8,8 @@ import {
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Highcharts from './../highcharts-base/HighchartsBase';
+import { primaryDarker } from '../../assets/themes/exilence-theme';
+import HC from 'highcharts';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   width: number;
@@ -27,7 +29,7 @@ export const useStyles = makeStyles((theme: Theme) =>
 
 const SnapshotHistoryChart: React.FC<Props> = ({
   chartData,
-  seriesName,
+  seriesName
 }: Props) => {
   const theme = useTheme();
   const classes = useStyles();
@@ -39,12 +41,49 @@ const SnapshotHistoryChart: React.FC<Props> = ({
     xAxis: {
       type: 'datetime'
     },
+    plotOptions: {
+      area: {
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops: [
+            [
+              0,
+              HC.color(theme.palette.primary.main)
+                .setOpacity(0.5)
+                .get('rgba')
+            ],
+            [
+              1,
+              HC.color(primaryDarker)
+                .setOpacity(0)
+                .get('rgba')
+            ]
+          ]
+        },
+        marker: {
+          radius: 2
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1
+          }
+        },
+        threshold: null
+      }
+    },
     series: [
       {
+        type: 'area',
         name: seriesName,
         data: chartData
       }
-    ],
+    ]
   };
 
   return (
