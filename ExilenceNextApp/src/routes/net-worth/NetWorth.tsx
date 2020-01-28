7870b4b3fc7fb205a91150599406684a32a87494
Petter Annerwall <petterannerwall@gmail.it>
2020-01-28 20:44:11 +0100

    Tooltip improvements

--- ExilenceNextApp/src/routes/net-worth/NetWorth.tsx
+++ ExilenceNextApp/src/routes/net-worth/NetWorth.tsx
@@ -1,26 +1,24 @@
-import { Grid, useTheme, Box, makeStyles, Theme } from '@material-ui/core';
-import GavelIcon from '@material-ui/icons/Gavel';
+import { Box, Grid, makeStyles, Theme, useTheme } from '@material-ui/core';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import UpdateIcon from '@material-ui/icons/Update';
 import { inject } from 'mobx-react';
 import { observer } from 'mobx-react-lite';
 import React, { useEffect } from 'react';
-import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import { useTranslation } from 'react-i18next';
 import { appName, visitor } from '../..';
+import DiscordLogo from '../../assets/img/discord-wordmark-colored.svg';
+import PatreonLogo from '../../assets/img/patreon-white.png';
 import { cardColors, itemColors } from '../../assets/themes/exilence-theme';
 import FeatureWrapper from '../../components/feature-wrapper/FeatureWrapper';
 import NetWorthTabGroup from '../../components/net-worth-tab-group/NetWorthTabGroup';
 import OverviewWidgetContent from '../../components/overview-widget-content/OverviewWidgetContent';
+import SnapshotHistoryChartContainer from '../../components/snapshot-history-chart/SnapshotHistoryChartContainer';
 import Widget from '../../components/widget/Widget';
 import { AccountStore } from '../../store/accountStore';
 import { SignalrStore } from '../../store/signalrStore';
 import { UiStateStore } from '../../store/uiStateStore';
-import SnapshotHistoryChartContainer from '../../components/snapshot-history-chart/SnapshotHistoryChartContainer';
-import DiscordLogo from '../../assets/img/discord-wordmark-colored.svg';
-import PatreonLogo from '../../assets/img/patreon-white.png';
 import { WindowUtils } from '../../utils/window.utils';
-import { useTranslation } from 'react-i18next';
-import moment from 'moment';
 
 interface NetWorthProps {
   accountStore?: AccountStore;
@@ -46,11 +44,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   }
 }));
 
-const NetWorth: React.FC<NetWorthProps> = ({
-  accountStore,
-  signalrStore,
-  uiStateStore
-}: NetWorthProps) => {
+const NetWorth: React.FC<NetWorthProps> = ({ accountStore, signalrStore, uiStateStore }: NetWorthProps) => {
   const classes = useStyles();
   const theme = useTheme();
   const activeProfile = accountStore!.getSelectedAccount.activeProfile;
@@ -77,9 +71,7 @@ const NetWorth: React.FC<NetWorthProps> = ({
   };
 
   const activeCurrency = () => {
-    return activeProfile
-      ? activeProfile.activeCurrency
-      : { name: 'chaos', short: 'c' };
+    return activeProfile ? activeProfile.activeCurrency : { name: 'chaos', short: 'c' };
   };
 
   const { activeGroup } = signalrStore!;
@@ -99,11 +91,7 @@ const NetWorth: React.FC<NetWorthProps> = ({
           <Widget backgroundColor={cardColors.primary}>
             <OverviewWidgetContent
               value={activeGroup ? activeGroup.netWorthValue : netWorthValue()}
-              secondaryValue={
-                activeGroup
-                  ? activeGroup.lastSnapshotChange
-                  : lastSnapshotChange()
-              }
+              secondaryValue={activeGroup ? activeGroup.lastSnapshotChange : lastSnapshotChange()}
               secondaryValueIsDiff
               secondaryValueStyles={{ fontSize: '0.8rem' }}
               title="label.total_value"
@@ -111,6 +99,7 @@ const NetWorth: React.FC<NetWorthProps> = ({
               currencyShort={activeCurrency().short}
               icon={<MonetizationOnIcon fontSize="default" />}
               currency
+              tooltip="Change in value between the two latest snapshots"
             />
           </Widget>
         </Grid>
@@ -131,43 +120,25 @@ const NetWorth: React.FC<NetWorthProps> = ({
         <Grid item xs={6} md={3} lg={3}>
           <Widget backgroundColor={cardColors.third}>
             <OverviewWidgetContent
-              value={
-                activeGroup
-                  ? activeGroup.groupSnapshots.length
-                  : snapshots().length
-              }
+              value={activeGroup ? activeGroup.groupSnapshots.length : snapshots().length}
               title="label.total_snapshots"
-              secondaryValue={
-                activeGroup
-                  ? activeGroup.timeSinceLastSnapshot
-                  : timeSinceLastSnapshot()
-              }
-              secondaryValueStyles={{ color: theme.palette.text.primary, fontSize: '0.8rem', fontWeight: 'normal'}}
+              secondaryValue={activeGroup ? activeGroup.timeSinceLastSnapshot : timeSinceLastSnapshot()}
+              secondaryValueStyles={{ color: theme.palette.text.primary, fontSize: '0.8rem', fontWeight: 'normal' }}
               valueColor={theme.palette.text.primary}
               icon={<UpdateIcon fontSize="default" />}
+              tooltip="Time since last snapshot"
             />
           </Widget>
         </Grid>
         <Grid item xs={6} md={3} lg={3}>
           <Widget>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              height={1}
-            >
-              <a
-                href="https://patreon.com/exilence"
-                onClick={e => WindowUtils.openLink(e)}
-              >
+            <Box display="flex" alignItems="center" justifyContent="center" height={1}>
+              <a href="https://patreon.com/exilence" onClick={e => WindowUtils.openLink(e)}>
                 <Box display="flex" alignItems="center" height={1}>
                   <img className={classes.patreonLogo} src={PatreonLogo} />
                 </Box>
               </a>
-              <a
-                href="https://discord.gg/yxuBrPY"
-                onClick={e => WindowUtils.openLink(e)}
-              >
+              <a href="https://discord.gg/yxuBrPY" onClick={e => WindowUtils.openLink(e)}>
                 <Box display="flex" alignItems="center" height={1}>
                   <img className={classes.discordLogo} src={DiscordLogo} />
                 </Box>
@@ -188,8 +159,4 @@ const NetWorth: React.FC<NetWorthProps> = ({
   );
 };
 
-export default inject(
-  'accountStore',
-  'signalrStore',
-  'uiStateStore'
-)(observer(NetWorth));
+export default inject('accountStore', 'signalrStore', 'uiStateStore')(observer(NetWorth));
