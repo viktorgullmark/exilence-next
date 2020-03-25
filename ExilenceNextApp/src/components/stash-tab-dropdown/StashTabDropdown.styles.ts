f3bead103ed9fd753256780badbc2843d87995ca
Viktor Gullmark <viktor.gullmark@gmail.com>
2020-03-26 00:07:14 +0100

    Improvements to filter and stash tab dropdown

--- ExilenceNextApp/src/components/stash-tab-dropdown/StashTabDropdown.styles.ts
+++ ExilenceNextApp/src/components/stash-tab-dropdown/StashTabDropdown.styles.ts
@@ -2,10 +2,6 @@ import { createStyles, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme =>
   createStyles({
-    formControl: {
-      marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(3)
-    },
     chip: {
       background: theme.palette.secondary.light
     },
