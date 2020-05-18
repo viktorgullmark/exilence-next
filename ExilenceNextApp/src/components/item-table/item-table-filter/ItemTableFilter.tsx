63d2701c6a110968f4572706a310ba9317657e4f
romankrru <rm07ru@gmail.com>
2020-05-19 01:05:12 +0300

    Prevent search form from being submitted

--- ExilenceNextApp/src/components/item-table/item-table-filter/ItemTableFilter.tsx
+++ ExilenceNextApp/src/components/item-table/item-table-filter/ItemTableFilter.tsx
@@ -1,5 +1,5 @@
 import { TextField, IconButton, Box } from '@material-ui/core';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import { observer } from 'mobx-react';
 import React, { ChangeEvent } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -25,64 +25,59 @@ const ItemTableFilter: React.FC<TableFilterProps<IPricedItem>> = ({
   const { t } = useTranslation();
   const classes = useStyles();
 
-  return (
-    <Formik
-      initialValues={{
-        searchText: ''
-      }}
-      onSubmit={(values: { searchText: string }) => {}}
-      validationSchema={Yup.object().shape({
-        searchText: Yup.string().max(20)
-      })}
-    >
-      {/* todo: refactor and use new formik */}
-      {(formProps: any) => {
-        const { values, setFieldValue, handleChange } = formProps;
+  const formik = useFormik({
+    initialValues: {
+      searchText: '',
+    },
 
-        return (
-          <form>
-            <TextField
-              margin="dense"
-              variant="outlined"
-              onChange={e => {
-                handleChange(e);
-                handleFilter(e);
-              }}
-              name="searchText"
-              placeholder={t('tables:label.search_text')}
-              className={classes.searchField}
-              value={values.searchText}
-              InputProps={{
-                startAdornment: (
-                  <Box mr={1} display="flex" justifyContent="center" alignItems="center">
-                    <SearchIcon />
-                  </Box>
-                ),
-                endAdornment: (
-                  <>
-                    {values.searchText !== '' && (
-                      <IconButton
-                        aria-label="help"
-                        title={t('label.clear_search_icon_title')}
-                        className={classes.clearIcon}
-                        edge="start"
-                        size="small"
-                        onClick={e => {
-                          setFieldValue('searchText', '');
-                          clearFilter();
-                        }}
-                      >
-                        <CloseIcon />
-                      </IconButton>
-                    )}
-                  </>
-                )
-              }}
-            />
-          </form>
-        );
-      }}
-    </Formik>
+    onSubmit: (): void => { /* do nothing */ },
+
+    validationSchema: Yup.object().shape({
+      searchText: Yup.string().max(20)
+    }),
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <TextField
+        margin="dense"
+        variant="outlined"
+        onChange={e => {
+          formik.handleChange(e);
+          handleFilter(e);
+        }}
+        name="searchText"
+        placeholder={t('tables:label.search_text')}
+        className={classes.searchField}
+        value={formik.values.searchText}
+        InputProps={{
+          startAdornment: (
+            <Box mr={1} display="flex" justifyContent="center" alignItems="center">
+              <SearchIcon />
+            </Box>
+          ),
+          endAdornment: (
+            <>
+              {formik.values.searchText !== '' && (
+                <IconButton
+                  aria-label="help"
+                  title={t('label.clear_search_icon_title')}
+                  className={classes.clearIcon}
+                  edge="start"
+                  size="small"
+                  onClick={() => {
+                    formik.setFieldValue('searchText', '');
+                    clearFilter();
+                  }}
+                >
+                  <CloseIcon />
+                </IconButton>
+              )}
+            </>
+          )
+        }}
+      />
+    </form>
   );
 };
 
