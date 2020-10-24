1e294fa2bfb96e981a27e4238b836a7ebcf1d3f2
Viktor Gullmark <viktor.gullmark@gmail.com>
2020-10-25 01:34:39 +0200

    Removed obsolete typings

--- ExilenceNextApp/src/typings.d.ts
+++ ExilenceNextApp/src/typings.d.ts
@@ -1,9 +1,3 @@
-declare module 'mobx-react' {
-  export function inject<D>(
-    mapStoreToProps: (store: any) => D
-  ): <A extends D>(component: React.ComponentType<A>) => React.FC<Omit<A, keyof D> & Partial<D>>;
-}
-
 /* SystemJS module definition */
 declare let nodeModule: NodeModule;
 interface NodeModule {
