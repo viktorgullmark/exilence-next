a0c4534092a1689d062c44881c1aaddcfb60db22
Viktor Gullmark <viktor.gullmark@gmail.com>
2020-10-27 01:13:50 +0100

    Update ItemTableFilterSubtotal.styles.ts

--- ExilenceNextApp/src/components/item-table/item-table-filter-subtotal/ItemTableFilterSubtotal.styles.ts
+++ ExilenceNextApp/src/components/item-table/item-table-filter-subtotal/ItemTableFilterSubtotal.styles.ts
@@ -6,6 +6,7 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.text.secondary,
     display: 'flex',
     borderRadius: 20,
+    background: theme.palette.background.default,
     border: `1px solid rgba(56, 56, 56, 1)`,
     alignItems: 'center',
     justifyContent: 'center',
