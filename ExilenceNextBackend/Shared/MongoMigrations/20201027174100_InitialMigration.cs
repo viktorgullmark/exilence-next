f5c212407d5345cb954c1182e24afb47c23ab38e
Petter Annerwall <petterannerwall@gmail.it>
2020-10-27 17:49:16 +0100

    Added working migration example

---
+++ ExilenceNextBackend/Shared/MongoMigrations/20201027174100_InitialMigration.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using MongoDBMigrations;
+
+namespace Shared.MongoMigrations
+{
+    public class InitialMigration : IMigration
+    {
+        public Version Version => new Version(1, 0, 0);
+        public string Name => "Initial Migration";
+        public void Up(IMongoDatabase database)
+        {
+            //database.RunCommand();
+        }
+
+        public void Down(IMongoDatabase database)
+        {
+        }
+    }
+}
