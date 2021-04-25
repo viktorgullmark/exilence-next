62ff03b859a891ae9084a9f71f65f101af770cba
Viktor Gullmark <viktor.gullmark@gmail.com>
2021-04-25 16:50:34 +0200

    Updated config

--- ExilenceNextApp/src/config/app.config.dev.ts
+++ ExilenceNextApp/src/config/app.config.dev.ts
@@ -2,7 +2,6 @@ const devConfig = {
   baseUrl: 'https://next.exilence.app',
   production: false,
   sentryBrowserDsn: undefined,
-  i18nUrl: '/i18n/{{lng}}/{{ns}}.json',
   trackingId: '',
   redirectUrl: 'http://localhost:65535',
   oauthUrl: 'https://www.pathofexile.com',
