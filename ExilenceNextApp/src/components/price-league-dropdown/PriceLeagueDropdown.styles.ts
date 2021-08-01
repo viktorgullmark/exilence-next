3bcfd673f1ae9d264a1f8977753fd1ca438dd22c
Sebastian Krzyżanowski <eskrzy@gmail.com>
2021-08-01 18:58:06 +0200

    fix(profile): filter royale league

--- ExilenceNextApp/src/components/price-league-dropdown/PriceLeagueDropdown.styles.ts
+++ ExilenceNextApp/src/components/price-league-dropdown/PriceLeagueDropdown.styles.ts
@@ -4,6 +4,14 @@ const useStyles = makeStyles(() => ({
   small: {
     fontSize: '0.875rem',
   },
+  icon: {
+    alignSelf: 'center',
+  },
+  pricingLeagueHelper: {
+    margin: 0,
+    marginTop: 5,
+    color: '#ffc107',
+  },
 }));
 
 export default useStyles;
