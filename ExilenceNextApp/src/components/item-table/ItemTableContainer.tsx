efe35cf4ab325bcc0a658d62d312eb1fd875b58e
Viktor Gullmark <viktor.gullmark@gmail.com>
2021-09-19 13:27:31 +0200

    Formatting of date for image generation

--- ExilenceNextApp/src/components/item-table/ItemTableContainer.tsx
+++ ExilenceNextApp/src/components/item-table/ItemTableContainer.tsx
@@ -247,7 +247,7 @@ const ItemTableContainer = ({
                   <Grid item id="items-table-generated" className={classes.generatedAt}>
                     <Typography variant="body2">{t('label.generated_at')}</Typography>&nbsp;
                     <b>
-                      <u>{moment().format()}</u>
+                      <u>{moment().utc().format('YYYY-MM-DD HH:MM')}</u>
                     </b>
                     &nbsp;
                     <Typography variant="body2">{t('label.generated_by')}.</Typography>&nbsp;
