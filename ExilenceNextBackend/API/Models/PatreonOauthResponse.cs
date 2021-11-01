2afa701ab714e037f20516455e147de9ec59f48a
Petter <petter@wehype.it>
2021-11-01 08:45:21 +0100

    Started patreon oauth integration

---
+++ ExilenceNextBackend/API/Models/PatreonOauthResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class PatreonOauthResponse
+    {
+        public string AccessToken { get; set; }
+        public int ExpiresIn { get; set; }
+        public string TokenType { get; set; }
+        public string Scope { get; set; }
+        public string RefreshToken { get; set; }
+        public string Version { get; set; }
+    }
+
+
+}
